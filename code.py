e0ab75579d877a429f1e0958a700e082be2a7b54
luke235422 <138612761+luke235422@users.noreply.github.com>
2026-08-24 21:24:14 +0200

    Delete code.py

--- code.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-pd.set_option('display.max_columns', None)
-
-accepted_file= pd.read_csv("accepted_2007_to_2018Q4.csv")
-rejected_file= pd.read_csv("rejected_2007_to_2018Q4.csv")
-
-print(rejected_file.columns)
-
